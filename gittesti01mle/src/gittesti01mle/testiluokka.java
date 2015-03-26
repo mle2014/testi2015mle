33068f082b1e66d276108121a70881b196c2504d
miksa <miksa007@gmail.com>
2015-03-26 09:41:47 +0200

    Uuden brachin eka commit

--- gittesti01mle/src/gittesti01mle/testiluokka.java
+++ gittesti01mle/src/gittesti01mle/testiluokka.java
@@ -1,12 +1,16 @@
 package gittesti01mle;
-
+/*
+ *@author miksa 
+ */
 public class testiluokka {
 
 	public static void main(String[] args) {
 		System.out.println("heippa");
 		System.out.println("Mika was here");
 		System.out.println("Petteri was here also");
-		
+		tulosta("Moro poro");
 	}
-
+	public static void tulosta(String teksti){
+		System.out.println(teksti);
+	}
 }
